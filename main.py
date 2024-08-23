c142168645860127ac713dc3c5951a97d1e6c014
Yadu <yadusasankan@gmail.com>
2024-08-23 15:10:44 +0530

    refactor: split into more functions add code specific to playlistitems (WIP)

--- main.py
+++ main.py
@@ -6,53 +6,78 @@
 
 import os
 
-import google_auth_oauthlib.flow
-import googleapiclient.discovery
-import googleapiclient.errors
+from google_auth_oauthlib.flow import InstalledAppFlow
+from googleapiclient.discovery import build
+from time import sleep
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
-CLIENT_SECRET_FILE = os.getenv('CLIENT_SECRET_FILE')
-API_KEY = os.getenv('API_KEY')
-CHANNEL_ID = os.getenv('CHANNEL_ID')
+CLIENT_SECRET_FILE = os.getenv("CLIENT_SECRET_FILE")
+API_KEY = os.getenv("API_KEY")
+CHANNEL_ID = os.getenv("CHANNEL_ID")
+PLAYLIST_ID = os.getenv("PLAYLIST_ID")
+
+SCOPES = ["https://www.googleapis.com/auth/youtube.readonly"]
+API_SUBSEQUENT_CALL_DELAY = 5  # seconds
+MAX_COUNT = 2
 
 
-scopes = ["https://www.googleapis.com/auth/youtube.readonly"]
-
-def main():
+def init_youtube_variables():
     # Disable OAuthlib's HTTPS verification when running locally.
     # *DO NOT* leave this option enabled in production.
     os.environ["OAUTHLIB_INSECURE_TRANSPORT"] = "1"
-
     api_service_name = "youtube"
     api_version = "v3"
-    client_secrets_file = CLIENT_SECRET_FILE
 
+    client_secrets_file = CLIENT_SECRET_FILE
     # Get credentials and create an API client
-    flow = google_auth_oauthlib.flow.InstalledAppFlow.from_client_secrets_file(
-        client_secrets_file, scopes)
-    
+    flow = InstalledAppFlow.from_client_secrets_file(client_secrets_file, SCOPES)
+
     # credentials = flow.run_console()
-    '''
+    """
     run_console command kept failing since that function was no longer supported; after digging around trying to fix the versions in which this was supported,
     found that google shows error in browser even after getting it to work with older versions.
 
-    with some help from chatGPT was able to figure out that flow using run_console is no longer supported, and we should instead use run_local_server, 
+    with some help from chatGPT was able to figure out that flow using run_console is no longer supported, and we should instead use run_local_server,
     which worked and was able to display channel details in console
-    '''
+    """
     credentials = flow.run_local_server(port=0)
-    youtube = googleapiclient.discovery.build(
-        api_service_name, api_version, credentials=credentials)
+    youtube = build(api_service_name, api_version, credentials=credentials)
+    return youtube
 
-    request = youtube.channels().list(
-        part="snippet,contentDetails,statistics",
-        mine=True
+
+def get_playlist_data(youtube, nextPageToken=None):
+    request = youtube.playlistItems().list(
+        part="snippet,contentDetails", playlistId=PLAYLIST_ID, pageToken=nextPageToken
     )
     response = request.execute()
+    return response
 
-    print(response)
+
+def main():
+    global MAX_COUNT
+
+    # counter added as a fallback to nextPageToken
+    counter = 0
+    nextPageToken = None
+
+    youtube = init_youtube_variables()
+    while counter < MAX_COUNT and nextPageToken != None:
+
+        response = get_playlist_data(youtube, nextPageToken)
+        nextPageToken = (
+            response["nextPageToken"] if "nextPageToken" in response else None
+        )
+
+        print(response)
+        print("\n=====================================================================")
+        print("nextPageToken: " + str(nextPageToken))
+        print("=====================================================================\n")
+        sleep(API_SUBSEQUENT_CALL_DELAY)
+        counter += 1
+
 
 if __name__ == "__main__":
     main()
